24ed4c5c1789828fcb453d2bc3d21811a30bdbeb
Tanawat Chansophonkul <tanawat.c@deeple.ai>
2022-09-24 18:54:07 +0700

    feat: add web-application firewall and instance

--- main.tf
+++ main.tf
@@ -53,3 +53,38 @@ resource "google_compute_instance" "web_public_instance" {
   }
 }
 
+
+################# web-application #################
+resource "google_compute_firewall" "web_application_firewall" {
+ name    = "web-application-firewall"
+ network = google_compute_network.web_network.name
+
+ allow {
+   protocol = "tcp"
+   ports    = ["8080"]
+ }
+
+ source_tags = ["web-public"]
+ target_tags = ["web-application"]
+}
+
+resource "google_compute_instance" "web_application_instance" {
+  name         = "web-application"
+  description  = "web-application allow traffic on port 8080 from web-public"
+  machine_type = "e2-medium"
+  zone         = "asia-southeast1-c"
+
+  tags = ["web-application"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.web_network.name
+    access_config {
+    }
+  }
+}
